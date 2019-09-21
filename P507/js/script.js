03bf5f5c4a33f9c98c74ae67320785088d4cc11d
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2019-09-21 20:04:16 +0400

    Add files via upload

---
+++ P507/js/script.js
@@ -0,0 +1,62 @@
+// var a = 3;
+// var b = 4;
+
+// a += 5; //a=a+5
+
+// a -= 2;
+// a++;
+// a--;
+
+// a **= 2;
+// console.log(a >= b);
+
+
+// var age = 16;
+// var city = "Sheki";
+
+// console.log(!(age >= 16 && city != "Baki"));
+
+// if (age >= 16) {
+//     console.log("Xosh gelmisiz,alish-verish ede bilersiz");
+// } else {
+//     console.log("Papani chagir");
+// }
+
+// var result = (age >= 16) ? "Xosh gelmisiz,alish-verish ede bilersiz" : "Papani chagir";
+// console.log(result)
+
+// var task = 40;
+// var project = 50;
+
+// var result = 0;
+
+// if (project >= 0 && project <= 100 && task <= 100 && task >= 0) {
+//     result = task * 0.4 + project * 0.6;
+//     if (result > 50) {
+//         console.log("Tebrikler");
+//     } else {
+//         console.log("Teessuf");
+//     }
+// } else {
+//     console.log("Zehmet olmasa 0-100 arasi eded daxil edin !!!");
+// }
+
+
+// var n = 10;
+
+// while (n < 10) {
+//     console.log(n);
+//     n++;
+// }
+
+// do {
+//     n++;
+//     console.log(n);
+
+// } while (n < 10)
+// var n = 10;
+// var m = 20;
+
+// for (var i = n; i <= m; i++) {
+//     console.log(i);
+// }
